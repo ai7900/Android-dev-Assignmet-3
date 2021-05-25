7b77ba2f03b5410b5428ed46b9a65a83e44542fb
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2021-05-25 13:21:08 +0200

    added basic UI

--- Project/app/src/main/java/com/example/dnd_char_creator/MainActivity.java
+++ Project/app/src/main/java/com/example/dnd_char_creator/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.dnd_char_creator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //getActionBar().show();
+        getSupportFragmentManager().beginTransaction().add((R.id.container),new Input(), null).commit();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu )
+    {
+        getMenuInflater().inflate( R.menu.menu_topdf, menu );
+        return true;
+    }
+
 }
